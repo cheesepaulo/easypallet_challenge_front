f3a0271e5b820bf0ae9b860b2ed8918b88b2663a
Paulo Cesar <pcdsjr@gmail.com>
2020-03-18 12:04:36 -0500

    create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <v-app></v-app>
+  <v-app>
+    <Navbar />
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import Navbar from "./components/layout/Navbar";
+
 export default {
   name: "App",
 
-  components: {}
+  components: { Navbar }
 };
 </script>

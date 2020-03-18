b6eb54094f975149276489493cb583bc5a1b7180
Paulo Cesar <pcdsjr@gmail.com>
2020-03-18 15:30:09 -0500

    create load_detail component

--- src/router.js
+++ src/router.js
@@ -4,10 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import LoadList from './components/loads/LoadList'
+import LoadDetail from './components/loads/LoadDetail'
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/loads', component: LoadList }
+    { path: '/loads', component: LoadList },
+    { path: '/loads/:id', component: LoadDetail, props: true },
   ]
 })

350efa7145135bed00d886a5717a8d6700c4453e
Paulo Cesar <pcdsjr@gmail.com>
2020-03-18 15:30:27 -0500

    include link to load detail component

--- src/components/loads/LoadList.vue
+++ src/components/loads/LoadList.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-simple-table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Code</th>
-        <th>Delivery Date</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="load in loads" :key="load.id">
-        <th>{{ load.id }}</th>
-        <th>{{ load.code }}</th>
-        <th>{{ load.delivery_date }}</th>
-        <th>
-          <router-link :to="{ path: 'load/'+ load.id}">Detail</router-link>
-        </th>
-      </tr>
-    </tbody>
-  </v-simple-table>
+  <v-container class="mt-5">
+    <h3>List of Loads</h3>
+    <v-simple-table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Code</th>
+          <th>Delivery Date</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="load in loads" :key="load.id">
+          <th>{{ load.id }}</th>
+          <th>{{ load.code }}</th>
+          <th>{{ load.delivery_date }}</th>
+          <th>
+            <v-btn text :to="/loads/+load.id">
+              <v-icon class="mr-2">mdi-format-list-bulleted-square</v-icon>View Orders
+            </v-btn>
+          </th>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </v-container>
 </template>
 
 <script>

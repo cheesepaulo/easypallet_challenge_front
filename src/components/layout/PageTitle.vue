81b194a4bc383b6adab654b6b207c6600fdab69b
Paulo Cesar <pcdsjr@gmail.com>
2020-03-20 15:34:58 -0500

    create page_title component

---
+++ src/components/layout/PageTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-title">
+    <h1>
+      <v-icon v-if="icon">{{ icon }}</v-icon>
+      {{ title }}
+    </h1>
+    <v-divider />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["icon", "title", "subtitle"]
+};
+</script>
+
+<style>
+.page-title h1 {
+  margin: 0px;
+}
+
+.page-title h2 {
+  color: #777;
+  margin-top: 5px;
+  font-size: 1.3rem;
+}
+</style>

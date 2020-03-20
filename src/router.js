aafab7710816d273c0bf09d764762da0f724d325
Paulo Cesar <pcdsjr@gmail.com>
2020-03-20 15:44:09 -0500

    remove old components

--- src/router.js
+++ src/router.js
@@ -5,14 +5,12 @@ Vue.use(Router)
 
 import Loads from './components/loads/Loads'
 import Load from './components/loads/Load'
-import NewLoad from './components/loads/NewLoad'
 import Products from './components/orders/Products'
 
 export default new Router({
   mode: 'history',
   routes: [
     { path: '/loads', component: Loads },
-    { path: '/loads/new', component: NewLoad },
     { path: '/loads/:id', component: Load, props: true },
     { path: '/orders/:order_id/products', name: "order_products", component: Products, props: true }
   ]

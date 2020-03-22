2f97af443ddbc903bf7d9c67148c44ee2b7b1bd8
Paulo Cesar <pcdsjr@gmail.com>
2020-03-22 10:45:06 -0500

    create BackButton component

---
+++ src/components/BackButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn class="ml-5 default mt-4" @click="$router.go(-1)">
+    <v-icon left>mdi-arrow-left</v-icon>Voltar
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "BackButton"
+};
+</script>
+
+<style>
+</style>
